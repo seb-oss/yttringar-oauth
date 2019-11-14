b1f07e2d386bdab9b138a6de60aa892f718cd291
Gråborg <mikael.graborg@seb.se>
2019-11-14 16:02:41 +0100

    chore: remove unnecessary config options

--- src/app-settings.ts
+++ src/app-settings.ts
@@ -1,18 +1,17 @@
-import { config } from "dotenv";
+import { config } from 'dotenv'
 
-config();
+config()
 if (
   !process.env.CLIENT_ID ||
   !process.env.CLIENT_SECRET ||
   !process.env.STATE_PASSWORD ||
-  !process.env.BOT_TOKEN ||
   !process.env.ORIGINS
 ) {
-  throw new Error("Missing app settings.");
+  throw new Error('Missing app settings.')
 }
 
 if (process.env.STATE_PASSWORD.length !== 32) {
-  throw new Error('"state-password" must be 32 characters.');
+  throw new Error('"state-password" must be 32 characters.')
 }
 
 export const settings = {
@@ -20,6 +19,6 @@ export const settings = {
   client_secret: process.env.CLIENT_SECRET,
   state_password: process.env.STATE_PASSWORD,
   bot_token: process.env.BOT_TOKEN,
-  origins: process.env.ORIGINS.split(",").map(x => x.trim()),
-  githubUrl: process.env.GITHUB_URL || "https://github.com"
-};
+  origins: process.env.ORIGINS.split(',').map(x => x.trim()),
+  githubUrl: process.env.GITHUB_URL || 'https://github.com'
+}
