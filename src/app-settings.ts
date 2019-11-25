26a5741fc56876a5e4bdb6a6d87530f6737fae12
Gråborg <mikael.graborg@seb.se>
2019-11-25 18:46:39 +0100

    chore: remove dead config setting

--- src/app-settings.ts
+++ src/app-settings.ts
@@ -18,7 +18,6 @@ export const settings = {
   client_id: process.env.CLIENT_ID,
   client_secret: process.env.CLIENT_SECRET,
   state_password: process.env.STATE_PASSWORD,
-  bot_token: process.env.BOT_TOKEN,
   origins: process.env.ORIGINS.split(',').map(x => x.trim()),
   githubUrl: process.env.GITHUB_URL || 'https://github.com'
 }
